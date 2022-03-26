cc2e69c89a7674149d5176495c4efb23628f417e
Matthieu Pernelle <matthieu.pernelle@gmail.com>
2022-03-26 16:31:02 +0100

    :lipstick: (layout)

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,48 @@
+import 'dart:async';
+
+import 'package:ferry/ferry.dart';
+import 'package:ferry_flutter/ferry_flutter.dart';
+import 'package:ferry_memory_leak/__generated__/episodes.data.gql.dart';
+import 'package:ferry_memory_leak/__generated__/episodes.req.gql.dart';
+import 'package:ferry_memory_leak/__generated__/episodes.var.gql.dart';
 import 'package:flutter/material.dart';
+import 'package:gql_http_link/gql_http_link.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+final defaultFetchPolicies = {
+  OperationType.query: FetchPolicy.CacheFirst,
+};
+
+final cache = Cache();
+
+final link = HttpLink("https://rickandmortyapi.com/graphql");
+
+final client = Client(
+  link: link,
+  cache: cache,
+  defaultFetchPolicies: defaultFetchPolicies,
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ferry memory leak',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Ferry memory leak'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +51,78 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  bool _displayEpisodes = true;
+  bool _isLooping = false;
+  void _toggleDisplayEpisodes() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _displayEpisodes = !_displayEpisodes;
+    });
+  }
+
+  void _loopToggleDisplayEpisodes() {
+    setState(() {
+      _isLooping = true;
+    });
+    Timer.periodic(const Duration(milliseconds: 10), (_) {
+      _toggleDisplayEpisodes();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                onPressed: _toggleDisplayEpisodes,
+                child: const Text("Toggle episodes"),
+              ),
+              if (!_isLooping)
+                ElevatedButton(
+                  onPressed: _loopToggleDisplayEpisodes,
+                  child: const Text("Trigger memory leak"),
+                ),
+            ],
+          ),
+          if (_displayEpisodes) const Expanded(child: EpisodesList()),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class EpisodesList extends StatelessWidget {
+  const EpisodesList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Operation(
+      client: client,
+      operationRequest: GgetEpisodesReq((b) => b.vars.page = 1),
+      builder: (
+        BuildContext context,
+        OperationResponse<GgetEpisodesData, GgetEpisodesVars>? response,
+        Object? error,
+      ) {
+        final data = response?.data;
+        if (data == null || (response?.loading ?? true)) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        return ListView.builder(
+          itemBuilder: ((context, index) => ListTile(
+                title: Text(data.episodes?.results?[index].name ?? ""),
+              )),
+        );
+      },
     );
   }
 }
